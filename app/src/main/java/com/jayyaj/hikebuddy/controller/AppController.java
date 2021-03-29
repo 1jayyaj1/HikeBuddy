fea2e4c7b53a395fca7ef40379081e8660cf28b3
1jayyaj1 <jayabisaad@gmail.com>
2021-03-29 11:20:28 -0400

    Set up Android volley and singleton pattern for the controller

---
+++ app/src/main/java/com/jayyaj/hikebuddy/controller/AppController.java
@@ -0,0 +1,33 @@
+package com.jayyaj.hikebuddy.controller;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+    private static AppController instance;
+    private RequestQueue requestQueue;
+
+    public static synchronized AppController getInstance() {
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> r) {
+        getRequestQueue().add(r);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+}
